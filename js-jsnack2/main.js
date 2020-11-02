128330cfc0d8856d15800a5117a84691ad889484
Francesco <francesco.savignano96@gmail.com>
2020-11-02 16:07:42 +0100

    tolto un commento sbagliato da jsnack2 e creato cartella jsnack3

--- js-jsnack2/main.js
+++ js-jsnack2/main.js
@@ -1,6 +1,5 @@
 //JSnack2: Creare un array di nomi e chiedere all'utente due numeri. Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
 
-//inizializzo array di oggetti
 $(document).ready(function(){
 
     //inizializzo un array di nomi
---
+++ js-jsnack3/main.js
@@ -0,0 +1,7 @@
+//JSnack3: A partire da un array di oggetti, creare una copia dell'array e aggiungere ai singoli elementi dell'array una nuova proprietà "position" che contiene una lettera casuale.
+
+$(document).ready(function(){
+
+    
+
+});
